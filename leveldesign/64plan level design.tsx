78041b00ab9baa0dd8f3b656785fb1c9a8abcc7b
Tom Ch <74670050+XeoPhantom@users.noreply.github.com>
2022-03-25 13:03:42 +0100

    aaaaaaaaaaaaa
    tuez moi

---
+++ leveldesign/64plan level design.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="64plan level design" tilewidth="64" tileheight="64" tilecount="27" columns="9">
+ <image source="../assets/plan level design.png" width="600" height="200"/>
+</tileset>
---
+++ leveldesign/plan level design.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="plan level design" tilewidth="48" tileheight="48" tilecount="48" columns="12">
+ <image source="../assets/plan level design.png" width="600" height="200"/>
+</tileset>
