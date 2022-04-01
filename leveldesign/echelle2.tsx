3deef558d683a55168d74d722399285684834687
Tom Ch <74670050+XeoPhantom@users.noreply.github.com>
2022-04-01 13:07:03 +0200

    01/04

---
+++ leveldesign/echelle2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="echelle2" tilewidth="48" tileheight="48" tilecount="36" columns="6">
+ <image source="../../../../Downloads/tuez moi1.png" width="288" height="288"/>
+</tileset>
---
+++ leveldesign/test echelle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="test echelle" tilewidth="48" tileheight="48" tilecount="114" columns="19">
+ <image source="../../../../Downloads/zeldalikV2 (1).png" width="914" height="318"/>
+</tileset>
